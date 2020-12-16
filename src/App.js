711c82dee5e0fe01656e408aae3206cba4c09f2b
Fernando Buccella <fer.buccella@gmail.com>
2020-12-16 10:28:01 -0300

    Remove unnecessary markup.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Timezone from './components/Timezone'
 
@@ -6,21 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
         <Timezone name="America/Buenos_Aires" localTime="11/06/2020 4:35PM" />
-
       </header>
     </div>
   );
